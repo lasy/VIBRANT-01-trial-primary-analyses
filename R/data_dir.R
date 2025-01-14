ca4b91e90a72b2c76c1f6fac78165cd9d17e2cf3
lauravermeren <laura.vermeren@uclouvain.be>
2025-01-14 13:17:37 +0100

    try to run code on new data

--- R/data_dir.R
+++ R/data_dir.R
@@ -6,12 +6,12 @@ data_dir <- function(data_source = c("simulated", "real")){
     if (str_detect(getwd(), "laurasymul"))
       data_dir <- "/Users/laurasymul/OneDrive - UCL/Academia/Research/VIBRANT data UCLouvain/"
     else if (str_detect(getwd(), "vermeren"))
-      stop("@LauraV: change the function data_dir() in `R/data_dir.R` to point to your own OneDrive data directory")
+      data_dir <- "/Users/lvermeren/OneDrive - UCL/VIBRANT data UCLouvain/"
     else
       stop("You need to specify the path to the data directory in `R/data_dir.R`")
 
     data_dir <- data_dir |> str_c("simulated data/")
-    data_dir <- fs::dir_ls(data_dir) |> sort(decreasing = TRUE) |> extract(1) |> str_c("/")
+    data_dir <- fs::dir_ls(data_dir) |> sort(decreasing = TRUE) |> magrittr::extract(1) |> str_c("/")
   } else {
     stop("The `R/data_dir.R` needs to be modified to specify the path to the real data directory.")
   }
@@ -24,8 +24,10 @@ dropbox_dir <- function(){
   if (str_detect(getwd(), "laurasymul"))
     dropbox_dir <- "/Users/laurasymul/Dropbox/Academia/Projects/VIBRANT Study Files/"
   else if (str_detect(getwd(), "vermeren"))
-    stop("@LauraV: change the function dropbox_dir() in `R/data_dir.R` to point to your Dropbox directory")
+    dropbox_dir <- "/Users/lvermeren/Dropbox/VIBRANT Study Files/"
   else
     stop("Hello! You need to specify the path to the data directory in the function `dropbox_dir` in the R/data_dir.R file.")
   dropbox_dir
 }
+
+
