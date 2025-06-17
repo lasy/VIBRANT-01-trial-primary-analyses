f3137ee56225115e8bd15da782eebde391b799b7
Laura Symul <laurasymul@gmail.com>
2025-06-17 11:59:16 +0200

    Added fisher tests and population flags

--- R/viz_and_table_functions.R
+++ R/viz_and_table_functions.R
@@ -19,60 +19,6 @@ get_arm_colors <- function(){
     )
 }
 
-get_taxa_color <- function(taxa){
-
-  tibble(taxa = taxa) |>
-    mutate(
-      color =
-        case_when(
-          str_detect(taxa, "non-LBP") ~ "goldenrod1",
-          str_detect(taxa, "LBP") ~ "darkorange1",
-          str_detect(taxa, "iners") ~ "seagreen3",
-          str_detect(taxa, "[nN]on [lL]actobacillus") ~ "gray40",
-          str_detect(taxa, "actobacillus") ~ "seagreen4"
-        )
-    ) |>
-    pull(color)
-
-}
-
-
-get_taxa_colors <- function(taxa) {
-  tibble(
-    taxa = taxa
-  ) |>
-    mutate(
-      cat =
-        case_when(
-          taxa == "Other" ~ "Other",
-          str_detect(taxa, "crispatus") ~ "crispatus",
-          str_detect(taxa, "LBP") ~ "LBP",
-          str_detect(taxa, "iner") ~ "iners",
-          str_detect(taxa, "revotella") ~ "prevotella",
-          str_detect(taxa, "ardnerella") ~ "gardnerella",
-          TRUE ~ "non lacto"
-        )
-    ) |>
-    group_by(cat) |>
-    mutate(
-      color =
-        case_when(
-          taxa == "Other" ~ "gray80",
-          str_detect(taxa, "LBP") ~ colorRampPalette(c("orangered1", "red4"))(n()),
-          str_detect(taxa, "crispatus") ~ "orange",
-          str_detect(taxa, "iner") ~ "green3",
-          str_detect(taxa, "jensenii") ~ "green4",
-          str_detect(taxa, "ardnerella") ~ colorRampPalette(c("dodgerblue1", "dodgerblue4"))(n()),
-          str_detect(taxa, "revotella") ~ colorRampPalette(c("purple1", "purple4"))(n()),
-          TRUE ~ colorRampPalette(c("turquoise3", "black"))(n())
-        )
-    ) |>
-    pull(color)
-}
-
-get_site_colors <- function(){
-  c("CAP" = "green4", "MGH" = "steelblue1")
-}
 
 get_origin_colors <- function(){
   c("SA" = "green3", "US" = "steelblue3", "Other" = "gray80")
